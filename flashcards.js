29a1ad3227d4d4643d0715ca35ec85d0c439e99a
maxpleaner <maxpleaner@gmail.com>
2016-02-17 13:33:34 -0800

    no debugger

--- flashcards.js
+++ flashcards.js
@@ -7,20 +7,16 @@ Flashcards = {
   setupAnswerButtonListeners: function() {
       var $base = $(this.baseClass)
       var $answerButton = $("<button class='answer-button'>answer</button>")
-      // debugger
       var $togglers = $base.find(this.answerButtonClasses).hide().before($answerButton)
       $(".answer-button").before($("<br>")).after($("<br>"))
       $base.find(".answer-button").on("click", this.togglerClicked)  
   },
 
   togglerClicked: function(e){
-      // debugger
       var $toggler = $(e.currentTarget).nextAll(Flashcards.answerButtonClasses).first()
       if ($toggler.hasClass("visible")) {
-          // debugger
           $toggler.hide().removeClass("visible")
       } else {
-          // debugger
           $toggler.show().addClass("visible")
       }
       e.preventDefault()
@@ -31,11 +27,9 @@ Flashcards = {
       $(e.preventDefault())
       var $toggler = $(e.currentTarget).parent().children(Flashcards.answerButtonClasses)
       if ($toggler.hasClass("visible")) {
-          // debugger
           $toggler.hide()
           $toggler.removeClass("visible")
       } else {
-          // debugger
           $toggler.addClass("visible")
           $toggler.show()
       }
